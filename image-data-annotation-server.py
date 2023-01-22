809f87b107cd9b6760942eeeff7c87570bbab56d
Israel Abebe <se.israel.abebe@gmail.com>
2023-01-22 14:34:33 +0100

    saves the annotations in username specified column rather than joining

--- image-data-annotation-server.py
+++ image-data-annotation-server.py
@@ -8,12 +8,16 @@ from telebot import types
 
 file_name = 'data/test.csv'
 
-def read_csv(file_name):
-    return pd.read_csv(file_name)
+def read_csv(file_name,user_name):
+    data = pd.read_csv(file_name)
+    data[user_name] = np.nan
+    return data
 
-def update_csv(file_name, image_id, choice):
+
+def update_csv(file_name, image_id, choice,user_name):
+    # print('updating')
     df = pd.read_csv(file_name)
-    df.loc[df['ID'] == image_id, 'choice'] = choice
+    df.loc[df['ID'] == image_id, user_name] = choice
     df.to_csv(file_name, index=False)
 
 
@@ -27,7 +31,7 @@ def start(message):
     if images_df.empty:
         bot.send_message(chat_id=message.chat.id, text="All images have been shown.")
     else:
-        unshown_images = images_df.loc[images_df['choice'].isnull()]
+        unshown_images = images_df.loc[images_df[user_name].isnull()]
         if unshown_images.empty:
             bot.send_message(chat_id=message.chat.id, text="All images have been shown.")
         else:
@@ -39,18 +43,22 @@ def start(message):
             keyboard.add(yes_button, no_button)
             bot.send_photo(chat_id=message.chat.id, photo=open(image['image'], 'rb'), caption=image['description'], reply_markup=keyboard)
 
+
+
 @bot.callback_query_handler(func=lambda call: call.data.startswith('yes_'))
 def callback_yes(call):
+    user_name = call.message.chat.first_name
     image_id = call.data.split('_')[1]
-    update_csv(file_name, int(image_id), 'yes')
-    bot.send_message(chat_id=call.message.chat.id, text="Thank you for your response!")
+    update_csv(file_name, int(image_id), choice='yes',user_name=user_name)
+    bot.send_message(chat_id=call.message.chat.id, text=f"Thank you for your response! {user_name}")
     start(call.message)
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('no_'))
 def callback_no(call):
+    user_name = call.message.chat.first_name
     image_id = call.data.split('_')[1]
-    update_csv(file_name, int(image_id), 'no')
-    bot.send_message(chat_id=call.message.chat.id, text="Thank you for your response!")
+    update_csv(file_name, int(image_id), choice='no',user_name=user_name)
+    bot.send_message(chat_id=call.message.chat.id, text=f"Thank you for your response! {user_name}")
     start(call.message)
 
 
